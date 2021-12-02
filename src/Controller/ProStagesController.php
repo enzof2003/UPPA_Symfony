5248c6457e0771be701eac401e8b59eef1de2b8d
Enzo Flamand <enzof2003@outlook.fr>
2021-12-02 09:13:36 +0100

    Fin TP1

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -9,11 +9,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/pro/stages", name="pro_stages")
+     * @Route("/", name="pro_stages-accueil")
      */
     public function index(): Response
     {
         return $this->render('pro_stages/index.html.twig', [
+            'controller_name' => 'ProStagesController',
+        ]);
+    }
+	/**
+     * @Route("/entreprises", name="pro_stages-entreprises")
+     */
+    public function entreprises(): Response
+    {
+        return $this->render('pro_stages/entreprises.html.twig', [
+            'controller_name' => 'ProStagesController',
+        ]);
+    }
+	
+		/**
+     * @Route("/formations", name="pro_stages-formations")
+     */
+    public function formations(): Response
+    {
+        return $this->render('pro_stages/formations.html.twig', [
+            'controller_name' => 'ProStagesController',
+        ]);
+    }
+	
+		/**
+     * @Route("/stages/{id}", name="pro_stages-stages")
+     */
+    public function stages(): Response
+    {
+        return $this->render('pro_stages/stages.html.twig', [
             'controller_name' => 'ProStagesController',
         ]);
     }
