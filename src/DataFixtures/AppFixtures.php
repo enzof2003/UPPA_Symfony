1d03727e96a8e62393356093a333193679e46aba
Enzo Flamand <enzof2003@outlook.fr>
2022-01-09 00:32:01 +0100

    Avant le massacre

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,12 +5,30 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use App\Entity\Stage;
+use App\Entity\Entreprise;
+use App\Entity\Formation;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //Générateur Faker
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $nbEntreprises = $faker->numberBetween($min = 5, $max = 10);
+        for($i = 0; $i < $nbEntreprises ; $i++)
+        {
+            $entreprise = new Entreprise();
+            $entreprise->setActivite($faker->sentence($nbWords=6,$variableNbWords=true));
+            $entreprise->setAdresse($faker->address);
+            $entreprise->nom($faker->company);
+            $entreprise->setURLSite($faker->domainName);
+        }
+
+        $product = new Product();
+
+        $manager->persist($product);
 
         $manager->flush();
     }
